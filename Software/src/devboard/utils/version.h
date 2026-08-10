8907803226e66201ea249c445134d43b47def494
Norman Gehrsitz <git@gehrsitz.eu>
2026-08-10 15:01:10 +0200

    Simplify and keep c version

--- Software/src/devboard/utils/version.h
+++ Software/src/devboard/utils/version.h
@@ -1,13 +1,24 @@
 #pragma once
 
 // The build script (tools/identify_build.py) generates version_autogen.h
-// with concrete #defines for BUILD_VERSION and the raw GIT_* / GITHUB_*
-// macros.  Fallback gracefully if the file isn't present for some reason.
+// with raw GIT_* / GITHUB_* defines. BUILD_VERSION is assembled below.
 
 #if __has_include("version_autogen.h")
 #include "version_autogen.h"
 #endif
 
-#ifndef BUILD_VERSION
+#if defined(GIT_TAG)
+#define BUILD_VERSION GIT_TAG
+
+#elif defined(GIT_ANCESTOR_TAG) && defined(GIT_SHORT_SHA)
+#if defined(GITHUB_PR) && defined(GITHUB_PR_HEAD_SHORT_SHA)
+#define BUILD_VERSION GIT_ANCESTOR_TAG "dev-" GITHUB_PR_HEAD_SHORT_SHA " (#" GITHUB_PR ")"
+#elif defined(GIT_BRANCH)
+#define BUILD_VERSION GIT_ANCESTOR_TAG "dev-" GIT_SHORT_SHA " (" GIT_BRANCH ")"
+#else
+#define BUILD_VERSION GIT_ANCESTOR_TAG "dev-" GIT_SHORT_SHA
+#endif
+
+#else
 #define BUILD_VERSION "unknown"
 #endif
